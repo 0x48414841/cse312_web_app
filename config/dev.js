b760aec64bd1bb2e055cece3197ba78a52353354
unknown <mafrank@buffalo.edu>
2021-02-25 19:48:03 -0500

    mongodb added for local testing

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,8 @@
 module.exports = {
     googleClientID: "490596678911-4pem6iidu8n48k8bup2fksk4rj2nt250.apps.googleusercontent.com",
     googleClientSecret: "hzXeRnwz1RAVSsCldc9f0grr",
-    mongoURI: "mongodb+srv://bailey8:100679Vcs-@email-app-prod.g0e20.mongodb.net/Email-App-Prod?retryWrites=true&w=majority",
+    //mongoURI: "mongodb+srv://bailey8:100679Vcs-@email-app-prod.g0e20.mongodb.net/Email-App-Prod?retryWrites=true&w=majority",
+    mongoURI: "mongodb://127.0.0.1:27017/db",
     cookieKey: "anything",
  }
 
